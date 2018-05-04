9fa5b0a3200bffd758558227d29e0338cdf7ea2d
Kadee1344 <kadee1344@gmail.com>
2018-05-04 10:30:49 +0300

    Creating a Layout Component

---
+++ src/containers/DessertBuilder/DessertBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class DessertBuilder extends Component {
+    render () {
+      return (
+        <Aux>
+          <div>Dessert</div>
+          <div>Build Controls</div>
+        </Aux>
+      );
+    }
+}
+
+export default DessertBuilder;
